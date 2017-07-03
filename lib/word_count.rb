7e800827d2dbde31a664b27e6f2a74acbd7b6d24
memetibryan <memetibryan@gmail.com>
2017-07-03 12:51:04 +0300

    added code to show whether the word is in the input sentence or not

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -4,37 +4,30 @@ class String
 
 	string1 = self.split
 	scope = string1.length
-	split_words = []
-	word = "boy"
+	@@split_words = []
+	
+	word_searcher = @@word
 	i = 0
 
 	while i < scope
-		split_words.push string1[i]
+		@@split_words.push string1[i]
 		i += 1
 	end
 
-    bank = split_words.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
+    hash = @@split_words.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
 
 	# dup = split_words.select{|element| string1.count(element) > 1 }
  #    numb.push dup.uniq.count
  #    return dup.uniq
+ # return @@word
+end
 
-
- if bank.include?(word)
-	   return bank.fetch(word)
+def search_string
+	
+ if @@split_words.include? (self)
+	        return "success"
 		else
 			return "no result"
 	end
- # return bank
-end
-
-# def search_string
-# 	# var = word_count
-# 	# word = self
-# 	# if var.include?(word)
-# 	#    return var.fetch(var.include?(word).key)
-# 	# 	else
-# 	# 		return "no result"
-# 	# end
-# 	end
+	end
 end
