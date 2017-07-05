cf54694306f56da3317e152adf787393bcd7a3db
memetibryan <memetibryan@gmail.com>
2017-07-05 13:01:29 +0300

    beautified my code in word_counnt.rb

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,25 +1,26 @@
 class String
-  def word_count
+    def word_count
 
-	string1 = self.split
-	scope = string1.length
-	@@split_words = []
+	    string1 = self.split
+	    scope = string1.length
+	    @@split_words = []
 	
-	i = 0
+	    i = 0
 
-	while i < scope
-		@@split_words.push string1[i]
-		i += 1
-	end
+	    while i < scope
+		    @@split_words.push string1[i]
+		    i += 1
+	    end
 
-    @@hash = @@split_words.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
-end
+        @@hash = @@split_words.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
+    end
 
-def search_string	
- if @@hash.keys.include? (self)
-	        return @@hash.fetch(self)
-		else
-			return "no result"
-	end
-	end
+
+    def search_string	
+        if @@hash.keys.include? (self)
+	            return @@hash.fetch(self)
+		    else
+		        return "no result"
+	    end
+    end
 end
